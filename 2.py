3134044f4f8c7b9b01d1cc5d3dc5854cf51a7ac9
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-10 12:55:06 +0545

    Remove embedded git repository

---
+++ 2.py
@@ -0,0 +1,23 @@
+student = "Python"
+rationg = 4.99
+is_Published = True
+course_name = "Python for begginners"
+
+message = """ this is used for 
+used for multiple lines of text
+"""
+
+# creating a function
+
+print (len(course_name))
+print (course_name[0])
+
+print (course_name[0:3])# 0,1,2.3
+print (course_name[:3]) # default 0
+
+print (course_name[3:])# default to end
+
+
+# \ escape sequence
+course_name = "Python for \"beginners\""
+print (course_name)
---
+++ 3.py
@@ -0,0 +1,11 @@
+# concatination
+
+first = "Name"
+second = "Second"
+full = f"{first} {second}"
+print(full)
+
+
+course = " Python for beginners"
+# . is used to access the methoods of string
+print(course.upper())
