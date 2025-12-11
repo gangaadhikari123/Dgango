ad89dee3bb0e8ada4d4c493804b557026b135a94
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-12-11 12:27:33 +0545

    basic python

--- 5.py
+++ 5.py
@@ -1,9 +1,4 @@
-# class myclass():
-#     def __len__(self):
-#         return 0
-    
-#     myobj =  myclass()
-#     print(bool(myobj))
+
 
 class myclass():
   def __len__(self):
@@ -23,3 +18,17 @@ print(myFunction())
 x = 200
 print(isinstance(x,int))# to check the data type of x
 
+
+
+numbers = [1,2,3,4,5]
+count = len(numbers)
+if count > 3:
+  print(f"List has {count} elements")
+  
+  if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")
+
+    x=5 
+    print(1 < x < 10)
+    print(1 < x and x < 10)
+    
---
+++ 6.py
@@ -0,0 +1,5 @@
+x =5 
+print(not(x > 3 and x < 10))
+
+# is is used to check if both variables point to the same object in memeory
+
